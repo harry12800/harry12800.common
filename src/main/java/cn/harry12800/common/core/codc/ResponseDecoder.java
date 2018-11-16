f22a74ab67186ec455bd00bca0f904bc20c72b04
wshua <sling@wshua.cc>
2018-11-16 11:15:40 +0800

    1

--- src/main/java/cn/harry12800/common/core/codc/ResponseDecoder.java
+++ src/main/java/cn/harry12800/common/core/codc/ResponseDecoder.java
@@ -14,7 +14,7 @@ import io.netty.handler.codec.ByteToMessageDecoder;
  * |  包头	|  模块号         |  命令号      |  结果码      |  长度          |   数据       |  
  * +——----——+——-----——+——----——+——----——+——----——+——----——+
  * </pre>
- * @author -harry12800-
+ * @author harry12800
  *
  */
 public class ResponseDecoder extends ByteToMessageDecoder {
