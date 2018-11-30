74837fecf98795c18a337de7db4e6666e66e79e6
wshua <sling@wshua.cc>
2018-11-30 19:41:49 +0800

    1

--- src/main/java/cn/harry12800/common/module/UserCmd.java
+++ src/main/java/cn/harry12800/common/module/UserCmd.java
@@ -1,5 +1,8 @@
 package cn.harry12800.common.module;
 
+import io.netty.util.NettyRuntime;
+import io.netty.util.internal.SystemPropertyUtil;
+
 /**
  * 用户模块
  * @author harry12800
@@ -18,4 +21,10 @@ public interface UserCmd {
 	short LOGIN = 2;
 	short SHOW_ALL_USER = 3;
 	short PULL_MSG = 4;
+	public static void main(String[] args) {
+		int x = Math.max(1, SystemPropertyUtil.getInt("io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
+		System.out.println(x);
+		 int y = NettyRuntime.availableProcessors() * 2;
+		 System.out.println(y);
+	}
 }
